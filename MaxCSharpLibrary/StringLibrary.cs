b81f30adee595740d183da3174f5c755ee43ccf4
Max Wong <max.wong@itrc.org.hk>
2019-10-10 14:39:04 +0800

    first commit

---
+++ MaxCSharpLibrary/StringLibrary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MaxCSharpLibrary
+{
+    public static class StringLibrary
+    {
+        //
+        // Summary:
+        //     Get the string between "pre" and "post" string at their first occurence
+        //
+        // Parameters:
+        //   pre:
+        //     The starting string
+        //   post:
+        //     The ending string
+        //
+        // Returns:
+        //     the string between "pre" and "post".
+        //     Example:
+        //       String: this is a [testing] [string].
+        //       pre: [
+        //       post: ]
+        //       return: testing
+        public static string StringBetweenFirstOccurence(this string str, string pre, string post)
+        {
+            if (str.IndexOf(pre) < 0)
+            {
+                throw new Exception("The starting string does not exists.");
+            }
+
+            if (str.IndexOf(post) < 0)
+            {
+                throw new Exception("The ending string does not exists.");
+            }
+
+            return str.Substring(str.IndexOf(pre) + pre.Length, str.IndexOf(post) - str.IndexOf(pre));
+        }
+    }
+}
